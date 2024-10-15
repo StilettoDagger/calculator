9762ae6558c790d586d6885732a1fc03f3d34cc7
StilettoDagger <159158452+StilettoDagger@users.noreply.github.com>
2024-10-15 16:48:37 +0300

    Add javascript with a calculator object that has basic operation methods

---
+++ app.js
@@ -0,0 +1,16 @@
+// Create a calculator object with operator methods
+
+const calculator = {
+    add(a, b) {
+        return a +b;
+    },
+    subtract(a, b) {
+        return a - b;
+    },
+    multiply(a, b) {
+        return a * b;
+    },
+    divide(a, b) {
+        return a / b;
+    },
+}
