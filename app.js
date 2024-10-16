15cfc80d68d40f7e54b7379e3f64d11ed4a1f594
StilettoDagger <159158452+StilettoDagger@users.noreply.github.com>
2024-10-16 10:49:35 +0300

    Refactor: reworked calculator logic to evaluate the whole expression properly

--- app.js
+++ app.js
@@ -2,11 +2,9 @@ const OPERATORS = ["+", "-", "*", "/"];
 
 // Create a calculator object with operator methods
 const calculator = {
-	query: "",
-	firstOperand: null,
-	secondOperand: null,
+	expression: "",
+	numInput: "",
 	result: null,
-	operator: "",
 	add(a, b) {
 		return a + b;
 	},
@@ -19,41 +17,59 @@ const calculator = {
 	divide(a, b) {
 		return a / b;
 	},
-	calculate() {
-		if (this.query === "") {
-			return;
-        }
-        else if (this.query && this.firstOperand === null)
-        {
-            this.firstOperand = +this.query;
-            this.result = this.firstOperand;
-            return;
-        }
-
-		this.secondOperand = +this.query;
-
-		switch (this.operator) {
+	operate(firstNum, secondNum, operator) {
+		switch (operator) {
 			case "+":
-				this.result = this.add(this.firstOperand, this.secondOperand);
-				break;
+				return this.add(firstNum, secondNum);
 			case "-":
-				this.result = this.subtract(this.firstOperand, this.secondOperand);
-				break;
+				return this.subtract(firstNum, secondNum);
 			case "*":
-				this.result = this.multiply(this.firstOperand, this.secondOperand);
-				break;
+				return this.multiply(firstNum, secondNum);
+
 			case "/":
-				this.result = this.divide(this.firstOperand, this.secondOperand);
+				return this.divide(firstNum, secondNum);
+
+			default:
+				return firstNum;
+		}
+	},
+	evalExpression(
+		type // Evaluate all expressions of a certain type
+	) {
+		let pattern;
+		switch (type) {
+			case "multDiv":
+				pattern = /(\d+\.?\d*)([\*\/])(\d+\.?\d*)/;
+				break;
+			case "addSub":
+				pattern = /(\d+\.?\d*)([\+\-])(\d+\.?\d*)/;
 				break;
 			default:
-                this.result = this.firstOperand;
-				break;
+				return;
 		}
 
-		if (this.result !== null) {
-			this.firstOperand = this.result;
-			this.secondOperand = null;
+		while (pattern.test(this.expression)) {
+			const match = this.expression.match(pattern); // This returns an array where the first number, operator, and second number are on the indices 1, 2, and 3 respectively
+
+			const expResult = this.operate(+match[1], +match[3], match[2]);
+
+			this.expression = this.expression.replace(pattern, expResult);
 		}
+	},
+	evaluateAllExpressions() {
+		// Evaluate the entire expression.
+
+		// First evaluate all multiplication and division expressions
+
+		this.evalExpression("multDiv");
+
+		// Then evaluate all addition and subtraction expressions
+
+		this.evalExpression("addSub");
+
+		// Store the final result.
+
+		this.result = this.expression;
 	},
 };
 
@@ -84,8 +100,15 @@ window.addEventListener("keydown", (e) => {
 
 	const keyPressed = e.key;
 
-	if (keyPressed === "Backspace" && calculator.query) {
-		calculator.query = calculator.query.slice(0, calculator.query.length - 1);
+	if (keyPressed === "Backspace" && calculator.expression) {
+		calculator.expression = calculator.expression.slice(
+			0,
+			calculator.expression.length - 1
+		);
+		calculator.numInput = calculator.numInput.slice(
+			0,
+			calculator.expression.length - 1
+		);
 		queryInput.textContent = queryInput.textContent.slice(
 			0,
 			queryInput.textContent.length - 1
@@ -94,19 +117,21 @@ window.addEventListener("keydown", (e) => {
 	}
 
 	if (keyPressed === "Enter" || keyPressed === "=") {
-		calculator.calculate();
-		calculator.query = "";
+		calculator.evaluateAllExpressions();
+		calculator.numInput = "";
 		queryInput.textContent = calculator.result;
 	}
 
 	if (regex.test(keyPressed)) {
+		calculator.expression += keyPressed;
+
 		if (OPERATORS.includes(keyPressed)) {
-			calculator.operator = keyPressed;
-            calculator.calculate();
-            calculator.query = "";
-		} else {
-			calculator.query += keyPressed;
+			calculator.numInput = "";
 		}
+
+        else {
+            calculator.numInput += keyPressed;
+        }
 
 		queryInput.textContent += keyPressed;
 		console.table(calculator);
